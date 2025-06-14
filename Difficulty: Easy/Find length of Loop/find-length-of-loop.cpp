5bf2429a14b3f5d2893d6332a2f90e0d42a4e0d0
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-06-15 00:40:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
@@ -0,0 +1,44 @@
+/*
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node* slow=head;
+        Node* fast=head;
+        
+        while(fast!=NULL&&fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(fast==slow)
+            {
+                break;
+            }
+        }
+        if(fast==NULL||fast->next==NULL)
+        return 0;
+        
+        int count=1;
+        slow=fast->next;
+        while(slow!=fast)
+        {
+            count++;
+            slow=slow->next;
+            
+        }
+        
+    return count;
+    }
+};
