6a5556bb20d4677aaefe2e74c5e0fd8fa0efb77f
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-12-16 21:25:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.c
@@ -0,0 +1,13 @@
+// User function Template for C
+
+int search(int arr[], int n, int x) {
+    // Code here
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
